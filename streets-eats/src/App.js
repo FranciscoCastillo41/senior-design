3dad8b903148a3e1a70be3045c3ba38936e1d8b7
Francisco <frajcastillo@gmail.com>
2024-02-11 13:13:14 -0600

    Updated App.js React Router Dom format

--- streets-eats/src/App.js
+++ streets-eats/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// Component imports
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+
+// Authentication imports
+import Signin from "./Auth/Signin";
+import Signup from "./Auth/Signup";
+
+// Page imports
+import Home from "./Pages/Home";
+import About from "./Pages/About";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
